e78373cfc25aabc0134ead41a164c68f8b06e5d3
Rafael E. Cortez <rafael.ecortez@gmail.com>
2024-08-27 15:09:15 -0300

    back: refactor(Entity Restaurant): modified

--- Backend/DeliMovil-API-Rest/src/main/java/com/delimovil/backend/models/entity/Restaurant.java
+++ Backend/DeliMovil-API-Rest/src/main/java/com/delimovil/backend/models/entity/Restaurant.java
@@ -1,12 +1,20 @@
 package com.delimovil.backend.models.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 
 @Entity(name = "restaurant")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
 public class Restaurant {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Integer id;
     @Column(length = 45)
     private String name;
