d561c3bc2cd6518f097cbab04e4aa5d716d959bd
Agustin Vessoni <agustinvessoni149@gmail.com>
2024-08-26 19:42:47 -0600

    creacion del .env y datos de la DB

--- Backend/DeliMovil-API-Rest/src/main/java/com/delimovil/backend/DeliMovilApiRestApplication.java
+++ Backend/DeliMovil-API-Rest/src/main/java/com/delimovil/backend/DeliMovilApiRestApplication.java
@@ -1,12 +1,19 @@
 package com.delimovil.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DeliMovilApiRestApplication {
 
+
     public static void main(String[] args) {
+
+        /* Las siguientes 2 líneas son para cargar las variables de entorno definidas en el archivo .env */
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(DeliMovilApiRestApplication.class, args);
     }
 
