f144191b80f87934969333c79ecc2565c0c22d1c
Rafael E. Cortez <rafael.ecortez@gmail.com>
2024-08-28 02:30:22 -0300

    back: refactor: add restaurantDTOs

---
+++ Backend/DeliMovil-API-Rest/src/main/java/com/delimovil/backend/dto/RestaurantDTO.java
@@ -0,0 +1,16 @@
+package com.delimovil.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RestaurantDTO {
+    private Integer id;
+    private String name;
+    private String description;
+}
---
+++ Backend/DeliMovil-API-Rest/src/main/java/com/delimovil/backend/dto/ResturantCreateDTO.java
@@ -0,0 +1,15 @@
+package com.delimovil.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResturantCreateDTO {
+    private String name;
+    private String description;
+}
