6801c1a2ee405b52ef0b69eee03555e7025a2b24
Sebastián Fretes <sebafretesca@gmail.com>
2024-08-29 09:04:13 -0400

    Register component w styles done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Register } from "./pages/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route></Route>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="" element={<Register />} />
         <Route></Route>
         <Route></Route>
         <Route></Route>
--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,6 +1,74 @@
+import { useState } from 'react';
+import '../index.css';
 
 export const Register = () => {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = () => {
+    setName('');
+    setEmail('');
+    setPassword('');
+  }
+
   return (
-    <div>Register</div>
+
+    <div>
+      <div className='container'>
+        <div>
+          <h1>Registráte</h1>
+          <form>
+            <div>
+              <label htmlFor="name" className="form-label">Nombre y Apellido</label>
+              <input
+                id="name"
+                type="text"
+                value={name || ''}
+                onChange={(e) => setName(e.target.value)}
+                className="form-control"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="username" className="form-label">Email</label>
+              <input
+                id="email"
+                type="email"
+                value={email || ''}
+                onChange={(e) => setEmail(e.target.value)}
+                className="form-control"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="form-label">Contraseña</label>
+              <input
+                id="password"
+                type="password"
+                value={password || ''}
+                onChange={(e) => setPassword(e.target.value)}
+                className="form-control"
+                required
+              />
+            </div>
+            
+            <div>
+              <button
+                type="button"
+                onClick={handleRegister}
+              >
+                REGISTRAR
+              </button>
+            </div>
+          </form>
+          <p>
+            ¿Ya tenés una cuenta?{" "}
+            Iniciá sesión
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
