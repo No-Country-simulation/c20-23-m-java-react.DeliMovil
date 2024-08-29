c4d2f61710b74926ef75b9add85c7c636cb8f1c7
Sebastián Fretes <sebafretesca@gmail.com>
2024-08-29 09:28:49 -0400

    login form done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
-        <Route path="" element={<Register />} />
-        <Route></Route>
+        {/* <Route path="" element={<Register />} /> */}
+        <Route path="" element={<Login />} />
         <Route></Route>
         <Route></Route>
         <Route></Route>
--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,6 +1,59 @@
+import { useState } from 'react';
+import '../index.css';
 
 export const Login = () => {
-  return (
-    <div>Login</div>
-  )
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        setEmail('');
+        setPassword('');
+    }
+
+
+    return (
+        <div className='container'>
+            <div>
+                <div>
+                    <h1>Iniciá sesión</h1>
+                    <form>
+                        <div>
+                            <label htmlFor="email" className="form-label">Email</label>
+                            <input
+                                id="email"
+                                type="email"
+                                value={email || ''}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="form-control"
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input
+                                id="password"
+                                type="password"
+                                value={password || ''}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="form-control"
+                                required
+                            />
+                        </div>
+                        <div>
+                            <button
+                                type="button"
+                                onClick={handleLogin}
+                            >
+                                Ingresar
+                            </button>
+                        </div>
+                    </form>
+                    <p>
+                        ¿Todavía no te registraste? Creá una cuenta{" "}
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
 }
