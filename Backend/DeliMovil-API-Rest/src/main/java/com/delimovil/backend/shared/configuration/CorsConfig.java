4159b072b232dba5e7a7cc75140250ffd36942cc
Rafael E. Cortez <rafael.ecortez@gmail.com>
2024-09-03 12:20:39 -0300

    back: fix: modified CORS

--- Backend/DeliMovil-API-Rest/src/main/java/com/delimovil/backend/shared/configuration/CorsConfig.java
+++ Backend/DeliMovil-API-Rest/src/main/java/com/delimovil/backend/shared/configuration/CorsConfig.java
@@ -10,8 +10,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*");
+        registry.addMapping("/*")
+                .allowedOrigins("http://localhost:5173")
+
+                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowedHeaders("")
+                .allowCredentials(true);
+
     }
 }
