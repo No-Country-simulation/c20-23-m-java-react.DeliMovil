18774866dd12c4e0fc438b1ec8cad3893154040f
Rafael E. Cortez <rafael.ecortez@gmail.com>
2024-09-04 22:39:20 -0300

    Revert "register component created"
    This reverts commit 05af0669ebb46ffa991dc8f61e536cd618632705.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 import { Home } from "./pages/Home";
 
 function App() {
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,3 +1,3 @@
 export function Home() {
-  return <h1>Hola</h1>;
+  return;
 }
