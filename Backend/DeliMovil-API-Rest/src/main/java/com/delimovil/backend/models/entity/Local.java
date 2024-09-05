775eed7b25bae48a9567b5bdd9aafecc227b2cb1
Rafael E. Cortez <rafael.ecortez@gmail.com>
2024-09-05 16:12:37 -0300

    back: feature(local): add CRUD

--- Backend/DeliMovil-API-Rest/src/main/java/com/delimovil/backend/models/entity/Local.java
+++ Backend/DeliMovil-API-Rest/src/main/java/com/delimovil/backend/models/entity/Local.java
@@ -16,13 +16,20 @@ public class Local {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Integer id;
+    @Column(length = 45)
     private String name;
+    @Column(length = 45)
     private String latitude;
+    @Column(length = 45)
     private String longitude;
+    @Column(length = 20)
     private String phone;
     @Column(name = "number_street")
     private Integer numberStreet;
-    @Column(name = "floor_department")
+    @Column(name = "floor_department", length = 5)
     private String floorDepartment;
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
     private boolean isOpen;
 }
