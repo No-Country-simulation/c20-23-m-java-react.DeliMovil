f388059e1e2f61091624fbe32277a75f9db0124c
ojedacarolina <prfabiana480@gmail.com>
2024-09-05 15:05:23 -0300

    Cambios en API Restaurant

--- Frontend/src/pages/Restaurant/RestaurantAPI.js
+++ Frontend/src/pages/Restaurant/RestaurantAPI.js
@@ -1,24 +1,21 @@
 import BASE_URL from "../../configAxios";
 
-
 export const getRestaurant = () => BASE_URL.get(`/api/restaurant`);
-/*
-export const deleteRestaurant = (idCategory) => BASE_URL.delete(`/api/restaurant/${idCategory}/`, {});
+
+export const deleteRestaurant = (id) =>
+  BASE_URL.delete(`/api/restaurant/${id}/`, {});
 
 export const getRestaurantById = (id) =>
-    BASE_URL.get(`/api/restaurant/${id}/`, {});
+  BASE_URL.get(`/api/restaurant/${id}/`, {});
 
-export const createRestaurant = (parent_id ,name, description) =>
-    BASE_URL.post(`/api/restaurant/`, {
-        parent_id,
-        name,
-        description
-    });
+export const createRestaurant = (name, description) =>
+  BASE_URL.post(`/api/restaurant`, {
+    name,
+    description,
+  });
 
-export const editRestaurants = (id, parent_id, name, description) =>
-    BASE_URL.put(`/api/restaurant/${id}/`, {
-        parent_id,
-        name,
-        description,
-    });
-    */
+export const editRestaurants = (id, name, description) =>
+  BASE_URL.patch(`/api/restaurant/${id}/`, {
+    name,
+    description,
+  });
