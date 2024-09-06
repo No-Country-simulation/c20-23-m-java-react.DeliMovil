926476d08e6591acc06302e4890b0fd76ec7449e
Agustin Vessoni <agustinvessoni149@gmail.com>
2024-09-05 18:38:38 -0600

    back: refactor(client): ClientLoginDTO created

--- Backend/DeliMovil-API-Rest/src/main/java/com/delimovil/backend/services/interfaces/IClientService.java
+++ Backend/DeliMovil-API-Rest/src/main/java/com/delimovil/backend/services/interfaces/IClientService.java
@@ -1,6 +1,7 @@
 package com.delimovil.backend.services.interfaces;
 
 import com.delimovil.backend.dto.ClientDTO;
+import com.delimovil.backend.dto.ClientLoginDTO;
 import com.delimovil.backend.dto.ClientRequestDTO;
 
 import java.util.List;
@@ -9,8 +10,8 @@ public interface IClientService {
 
     public List<ClientDTO> findAll();
     public ClientDTO findById(Integer id);
-    public ClientDTO save(ClientRequestDTO clientDTO);
-    public ClientDTO update(ClientRequestDTO client, Integer id);
+    public ClientDTO save(ClientLoginDTO clientDTO);
+    public ClientDTO update(ClientRequestDTO clientDTO, Integer id);
     public void deleteById(Integer id);
 
 
