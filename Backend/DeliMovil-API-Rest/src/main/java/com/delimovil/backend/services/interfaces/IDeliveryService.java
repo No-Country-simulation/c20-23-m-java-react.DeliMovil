f8299bec9c1c39b5644cd840d1ccac56437ba153
Agustin Vessoni <agustinvessoni149@gmail.com>
2024-09-11 08:11:03 -0600

    back: feat(delivery): cloudinary implemented

--- Backend/DeliMovil-API-Rest/src/main/java/com/delimovil/backend/services/interfaces/IDeliveryService.java
+++ Backend/DeliMovil-API-Rest/src/main/java/com/delimovil/backend/services/interfaces/IDeliveryService.java
@@ -3,13 +3,14 @@ package com.delimovil.backend.services.interfaces;
 import com.delimovil.backend.dto.DeliveryDTO;
 import com.delimovil.backend.dto.DeliveryLoginDTO;
 import com.delimovil.backend.dto.DeliveryRequestDTO;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface IDeliveryService {
     public List<DeliveryDTO> findAll();
     public DeliveryDTO findById(Integer id);
-    public DeliveryDTO save(DeliveryRequestDTO deliveryDTO);
-    public DeliveryDTO update(DeliveryRequestDTO deliveryDTO, Integer id);
+    public DeliveryDTO save(DeliveryRequestDTO deliveryDTO, MultipartFile image);
+    public DeliveryDTO update(DeliveryRequestDTO deliveryDTO, MultipartFile image, Integer id);
     public void deleteById(Integer id);
 }
